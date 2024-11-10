646bb072f12dfef84a987dc32af31b22c729ed23
Anish-dev32 <anishkumar.dev32@gmail.com>
2024-11-10 08:38:48 +0000

    updated conversion and datatype

--- 01_basics/conversion_ops.js
+++ 01_basics/conversion_ops.js
@@ -1,17 +1,17 @@
 let score = "33"
-console.log(typeof score) //String
+//console.log(typeof score) //String
 let value = Number(score)
-console.log(typeof(value)) // Number
+//console.log(typeof(value)) // Number
 
 let value1 = "233abc"
 let value2 = Number(value1)  
-console.log(value2)     // NaN(Not in Number) -> case to be checked
-console.log(typeof(value2)) // Number
+//console.log(value2)     // NaN(Not in Number) -> case to be checked
+//console.log(typeof(value2)) // Number
 
 let value3 = null
 let value4 = Number(value3)  
-console.log(value4)     // 0 -> case to be checked
-console.log(typeof(value4)) // Number
+//console.log(value4)     // 0 -> case to be checked
+//console.log(typeof(value4)) // Number
 
 //same in case of undefined, value will be converted as NaN
 //true -> 1
@@ -19,16 +19,36 @@ console.log(typeof(value4)) // Number
 
 let isLoggedIn = 1;
 let booleanVal = Boolean(isLoggedIn)
-console.log(booleanVal) // true
+//console.log(booleanVal) // true
 
 isLoggedIn = "";
 booleanVal = Boolean(isLoggedIn)
-console.log(booleanVal) // false
+//console.log(booleanVal) // false
 
 isLoggedIn = "Anish";
 booleanVal = Boolean(isLoggedIn)
-console.log(booleanVal) // true
+//console.log(booleanVal) // true
 
 isLoggedIn = 0;
 booleanVal = Boolean(isLoggedIn)
-console.log(booleanVal) // false
+//console.log(booleanVal) // false
+
+let val = 43435
+val = -val
+//console.log(val)
+
+let fname = "Anish"
+let lname = " kumar"
+let flname = fname + lname;
+//console.log(flname)
+
+// console.log(1+"2") //12
+// console.log("1"+"2") //12
+// console.log(1+4+"5") //55
+
+let counter = 100
+// console.log(++counter) //101
+// console.log(counter++) //101
+
+console.log("2" == 2) //true -> compares value
+console.log("2" === 2) // false -> compares value as well as datatypes
--- 01_basics/data_Types.js
+++ 01_basics/data_Types.js
@@ -5,10 +5,28 @@ let name = "Anish" //String can be in '' or ""
 let age = 43 // number -> 2 to power 53
 let isLogggedIn = false
 
-console.log(typeof age) //number
+//console.log(typeof age) //number
 
 //null -> Standalone value ; type -> object
 //undefined -> not yet assigned
 //symbol -> unique
 //object
 
+//Primitive Datatype - call by value
+// String, Number, Boolean, null, undefined, Symbol, BigInt
+
+// Reference (Non Primitive) - Arrays, Objects, Functions
+
+const id = Symbol('2323')
+const anotherId = Symbol('2323')
+console.log(id === anotherId)//false
+
+const heros = ["Atal","Abdul"] //Array
+let myObj = {           //Object
+    name:"anish",
+    age:34
+}
+
+const fun = function(){ //function
+    console.log("Dummy")
+}
